c447a3cc4a7042ab45569b240c155d7ab9298182
Hayden Braxton <hayden.braxton.dev@gmail.com>
2018-07-09 22:27:52 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { chain, sample } from 'lodash';
+import './index.css';
+import AuthorQuiz from './AuthorQuiz/AuthorQuiz';
+import registerServiceWorker from './registerServiceWorker';
+
+const authors = [
+	{
+		name: 'Mark Twain',
+		imageUrl: 'images/authors/marktwain.jpg',
+		imageSource: 'Wikimedia Commons',
+		books: [ 'The Adventures of Huckleberry Finn' ]
+	},
+	{
+		name: 'Joseph Conrad',
+		imageUrl: 'images/authors/marktwain.jpg',
+		imageSource: 'Wikimedia Commons',
+		books: ['Heart of Darkness']
+	},
+	{
+		name: 'J.K. Rowling',
+		imageUrl: 'images/authors/jkrowling.jpg',
+		imageSource: 'Wikimedia Commons',
+		books: ['Harry Potter and the Sorcerers Stone']
+	},
+	{
+		name: 'Stephen King',
+		imageUrl: 'images/authors/stephenking.jpg',
+		imageSource: 'Wikimedia Commons',
+		books: ['The Shining', 'IT']
+	},
+	{
+		name: 'Charles Dickens',
+		imageUrl: 'images/authors/charlesdickens.jpg',
+		imageSource: 'Wikimedia Commons',
+		books: ['David Copperfield', 'A Tale of Two Cities']
+	},
+	{
+		name: 'William Shakespeare',
+		imageUrl: 'images/authors/williamshakespeare.jpg',
+		imageSource: 'Wikimedia Commons',
+		books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+	},
+];
+
+function getTurnBooks(authors) {
+	return chain(authors)
+		.reduce((books, author) => books.concat(author.books), [])
+		.shuffle()
+		.slice(0, 4)
+		.value();
+}
+
+function getTurnData(authors) {
+	const books = getTurnBooks(authors);
+	const answer = sample(books);
+	const author = authors.find((a) => a.books.includes(answer));
+
+	return { books, author };
+}
+
+const state = {
+	turnData: getTurnData(authors)
+};
+
+const element = <AuthorQuiz {...state}/>
+const root = document.getElementById('root');
+
+ReactDOM.render(element, root);
+registerServiceWorker();
